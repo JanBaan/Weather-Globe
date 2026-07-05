c6eaeedd45f5d7aac8a07683c5c592edfcabecdf
Jan <jwbaan98@gmail.com>
2026-07-05 22:44:55 +0200

    found a better csv file

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -4,11 +4,16 @@ extends Camera2D
 func _ready() -> void:
 	pass # Replace with function body.
 		
-func _physics_process(delta):	
+func _physics_process(delta):
+	drag()
 	zoom()
 	
+# this controls the zoom of the camera
 func zoom():
-	if Input.is_action_just_released('wheel down'):
+	if Input.is_action_just_released('wheel down') and get_zoom().x >= 1.5:
 		set_zoom(get_zoom() - Vector2(0.5, 0.5))
 	if Input.is_action_just_released('wheel up'):
 		set_zoom(get_zoom() + Vector2(0.5, 0.5))
+		
+func drag():
+	pass
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var weatherHistory: Array<Dictionary>
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +10,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func readWeatherData():
+	var file = FileAccess.open("res://csv/GlobalWeatherRepository.csv", FileAccess.READ)
